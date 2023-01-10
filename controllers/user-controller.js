cf7e0fe27cd3ab361679f5cceeab0698788a6867
LoisChen68 <a8283626@gmail.com>
2023-01-11 07:11:06 +0800

    feat: add signUp

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,50 @@
+const bcrypt = require('bcryptjs')
+const { User } = require('../models')
+
 const userController = {
   register: (req, res, next) => {
-    res.json('register')
+    const { role, email, account, password, confirmPassword } = req.body
+
+    if (!role || !email || !account || !password || !confirmPassword) {
+      return res.status(400).json({
+        type: 'Register failed',
+        title: 'Missing required data',
+        field_errors: {
+          role: 'required',
+          email: 'required',
+          account: 'required',
+          password: 'required',
+          confirmPassword: 'required',
+        },
+      })
+    }
+    User.findOne({
+      attributes: ['email'],
+      where: { email }
+    })
+      .then(user => {
+        if (!user) return bcrypt.hash(password, 10)
+        if (user.email === email) {
+          return res.status(400).json({
+            type: 'Register failed',
+            title: 'Email is used',
+            field_errors: {
+              email: 'used',
+            }
+          })
+        }
+      })
+      .then(hash => User.create({
+        role,
+        email,
+        account,
+        password: hash,
+        approvalStatus: 'reviewing',
+        isAdmin: false,
+        isSuspended: false,
+      }))
+      .then(() => res.json({ status: 'success', message: '註冊成功' }))
+      .catch(err => next(err))
   },
   login: (req, res, next) => {
     res.json('login')
--- models/user.js
+++ models/user.js
@@ -31,7 +31,7 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
     tableName: 'Users',
-    underscored: true,
+    underscored: false,
   });
   return User;
 };
