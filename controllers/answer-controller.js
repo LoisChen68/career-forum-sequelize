e72473a90b1433fb20e2eb97d4d4baddfb987965
LoisChen68 <a8283626@gmail.com>
2023-01-12 02:22:14 +0800

    feat: add getAnswer, putAnswer, deleteAnswer

--- controllers/answer-controller.js
+++ controllers/answer-controller.js
@@ -1,17 +1,69 @@
 const { Answer } = require('../models')
 
 const answerController = {
-  getAnswers: (req, res, next) => {
-    res.json('getAnswers')
-  },
+  // GET api/answers/:id 取得單筆回答
   getAnswer: (req, res, next) => {
-    res.json('getAnswer')
+    const answerId = req.params.id
+    Answer.findByPk(answerId)
+      .then(answer => {
+        if (!answer) return res.status(404).json({
+          status: 'error',
+          message: 'The answer is not found.'
+        })
+        res.json(answer)
+      })
+      .catch(err => next(err))
   },
+  // PUT api/answers/:id 修改回答
   putAnswer: (req, res, next) => {
-    res.json('putAnswer')
+    const userId = req.user.id
+    const answerId = req.params.id
+    const { content } = req.body
+    Answer.findByPk(answerId)
+      .then(answer => {
+        if (!answer) return res.status(404).json({
+          status: 'error',
+          message: 'The answer is not found.'
+        })
+        if (answer.userId !== userId) return res.status(403).json({
+          status: 'error',
+          message: 'Permission denied.'
+        })
+        if (!content) return res.status(400).json({
+          status: 'error',
+          message: 'Content is required.'
+        })
+        answer.update({ content })
+        return res.json({
+          status: 'success',
+          message: '成功修改回答',
+          answer
+        })
+      })
+      .catch(err => next(err))
   },
+  // DELETE api/answers/:id 刪除回答
   deleteAnswer: (req, res, next) => {
-    res.json('deleteAnswer')
+    const userId = req.user.id
+    const answerId = req.params.id
+    Answer.findByPk(answerId)
+      .then(answer => {
+        if (!answer) return res.status(404).json({
+          status: 'error',
+          message: 'The answer is not found.'
+        })
+        if (answer.userId !== userId) return res.status(403).json({
+          status: 'error',
+          message: 'Permission denied.'
+        })
+        return answer.destroy()
+      })
+      .then(answer => res.json({
+        status: 'success',
+        message: '成功刪除回答',
+        answer
+      }))
+      .catch(err => next(err))
   }
 }
 
--- routes/modules/answer.js
+++ routes/modules/answer.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const answerController = require('../../controllers/answer-controller')
 
-router.get('/', answerController.getAnswers)
 router.get('/:id', answerController.getAnswer)
 router.put('/:id', answerController.putAnswer)
 router.delete('/:id', answerController.deleteAnswer)
