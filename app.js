8d872623dac69915d30b143fe90eb1f6a7e23abb
LoisChen68 <a8283626@gmail.com>
2023-01-12 03:06:21 +0800

    feat: add cors & add package scripts "migrate"

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const passport = require('./config/passport')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(passport.initialize())
 
+app.use(cors())
 app.use('/api', routes)
 
 app.listen(PORT, () => {
