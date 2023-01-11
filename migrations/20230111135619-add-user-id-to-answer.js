772ed896683f9936eb70b76f66ee655055db9567
LoisChen68 <a8283626@gmail.com>
2023-01-11 22:02:14 +0800

    feat: add Answer associate User

---
+++ migrations/20230111135619-add-user-id-to-answer.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('Answers', 'userId', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Answers', 'userId')
+  }
+};
--- models/user.js
+++ models/user.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Question, { foreignKey: 'userId' })
+      User.hasMany(models.Answer, { foreignKey: 'userId' })
     }
   }
   User.init({
