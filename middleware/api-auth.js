1cf5908a3f8003f8f28fc1302ed2bdde5ba933e6
LoisChen68 <a8283626@gmail.com>
2023-01-12 02:45:57 +0800

    feat: add authenticatedAdmin, authApprovalStatus

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -1,8 +1,7 @@
 const passport = require('../config/passport')
-
 const authenticated = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (err || !user) return res.status(401).json({ status: 'error', message: '尚未登入' })
+    if (err || !user) return res.status(401).json({ status: 'error', title: 'Unauthorized request' })
     if (user) {
       req.user = user
     }
@@ -10,6 +9,22 @@ const authenticated = (req, res, next) => {
   })(req, res, next)
 }
 
+// 驗證 Admin
+const authenticatedAdmin = (req, res, next) => {
+  const reqUser = req.user
+  if (reqUser && reqUser.isAdmin) return next()
+  return res.status(403).json({ status: 'error', title: 'Unauthorized request' })
+}
+
+// 驗證使用者審核狀態是否為 approved
+const authApprovalStatus = (req, res, next) => {
+  const reqUser = req.user
+  if (reqUser && reqUser.approvalStatus === 'approved') return next()
+  return res.status(403).json({ status: 'error', title: 'Unapproved user' })
+}
+
 module.exports = {
-  authenticated
+  authenticated,
+  authenticatedAdmin,
+  authApprovalStatus
 }
--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,14 @@ const answer = require('./modules/answer')
 const userController = require('../controllers/user-controller')
 const passport = require('../config/passport')
 
-const { authenticated } = require('../middleware/api-auth')
+const { authenticated, authenticatedAdmin, authApprovalStatus } = require('../middleware/api-auth')
 const { apiErrorHandler } = require('../middleware/error-handle')
 
 router.post('/users/login', passport.authenticate('local', { session: false }), userController.login)
 router.post('/users/register', userController.register)
 
-router.use('/users', authenticated, users)
-router.use('/admin', admin)
+router.use('/users', authenticated, authApprovalStatus, users)
+router.use('/admin', authenticated, authenticatedAdmin, admin)
 router.use('/questions', authenticated, question)
 router.use('/answers', authenticated, answer)
 router.use('/', apiErrorHandler)
